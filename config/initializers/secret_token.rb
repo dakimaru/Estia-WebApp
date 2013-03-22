3db29ed1f0f22c95c0d19e7025ebac21cacc6782
Dimitri Akimaru <dimitri.akimaru@gmail.com>
2013-03-22 03:20:50 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Estia::Application.config.secret_token = '0ef7c0e9fdce8b70d4115b12d42305a9687e6c464c708ca81fea150180ee97a5007174cd47e703a2ca6d9ad6d379700541bb7f83c711d93b5fd185dbabe6e2b6'
