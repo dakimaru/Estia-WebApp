1f451bd3107421070783f6cd79b30e7017a584c3
Dimitri Akimaru <dimitri.akimaru@gmail.com>
2013-04-29 03:56:45 -0400

    logo redirects to homepage again

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,8 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
-    if current_user
-      redirect_to current_user
-    end
+    #if current_user
+    #  redirect_to current_user
+    #end
   end
 
   def about
